44e4a994b4608fc704d09f147eb7c88804d9352f
Oliver Aiaste <oliver.aiaste@perg.ee>
2026-03-19 11:06:48 +0200

    added shrekoclicker

---
+++ script.js
@@ -0,0 +1,19 @@
+if (localStorage.counter) {
+  document.getElementById("shrekocounter").innerHTML = localStorage.getItem("counter");
+} else {
+  document.getElementById("shrekocounter").innerHTML = 0;
+}
+let i = Number(document.getElementById("shrekocounter").innerHTML)
+
+function counter(){
+    i ++;
+    document.getElementById("shrekocounter").innerHTML = i;
+    localStorage.setItem("counter", i)
+}
+
+console.log("Hello, World! said the newborn website")
+//alert("Hello, World!")ˇ
+
+while (KeyboardEvent.ctrlKey) {
+    counter()
+}
